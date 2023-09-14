69d300052c3312f8211c85a5c26ffa2227529a62
k8 <katesterchi@gmail.com>
2023-09-14 03:23:02 +0200

    added association methods to my sequelize models

--- models/index.js
+++ models/index.js
@@ -4,13 +4,25 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Define associations
+Product.belongsTo(Category, {
+  foreignKey: 'category_id', // This is the foreign key in the Product table
+  onDelete: 'CASCADE', // Cascade delete if a category is deleted
+});
 
-// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id', // This should match the foreign key in the Product table
+});
 
-// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag, // The through model that connects Products and Tags
+  foreignKey: 'product_id', // Foreign key in the ProductTag table
+});
 
-// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag, // The through model that connects Products and Tags
+  foreignKey: 'tag_id', // Foreign key in the ProductTag table
+});
 
 module.exports = {
   Product,
